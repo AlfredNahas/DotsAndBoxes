55829d79693c0395192a5a22227ad210092d9122
Jado233 <jna35@mail.aub.edu>
2025-03-04 21:28:20 +0200

    here i made a new void func to print the grid, also enumerated each row and column of the grid for readability (for the game to be playable later on too) per the teachers request and modifyied the number of columns to 6 to match the teachers example. also i spaced out the grid a bit to make it look nicer

--- main.c
+++ main.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #define Rows 5
-#define Cols 4
+#define Cols 6
 
-
-void Gridbuilder (char grid[Rows][Cols])
+void Gridbuilder(char grid[Rows][Cols])
 {
-    int i, j;
-    for (i = 0; i < Rows; i++)
+    for (int i = 0; i < Rows; i++)
     {
-        for (j = 0; j < Cols; j++)
+        for (int j = 0; j < Cols; j++)
         {
-            grid[i][j] = ' ';
+            grid[i][j] = '.';
         }
     }
 }
 
+void printGrid(char grid[Rows][Cols])
+{
+    printf("   ");
+    for (int j = 0; j < Cols; j++)
+    {
+        printf(" %d ", j);
+    }
+    printf("\n");
 
-
-
+    for (int i = 0; i < Rows; i++)
+    {
+        printf(" %d ", i);
+        for (int j = 0; j < Cols; j++)
+        {
+            printf(" %c ", grid[i][j]);
+        }
+        printf("\n\n");
+    }
+}
 
 int main()
 {
+    char grid[Rows][Cols];
+    Gridbuilder(grid);
+    printGrid(grid);
 
+    return 0;
 }
