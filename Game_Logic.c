7de16fc9caa927c15a8c6c1c14da0026d27942db
Jado233 <jna35@mail.aub.edu>
2025-03-08 20:47:12 +0200

    added int scoreA and scoreB in the struct to track the score of each player and the bool method "checkForCompletedBoxes" for checking boxes and counting scores.

--- Game_Logic.c
+++ Game_Logic.c
@@ -8,6 +8,8 @@ typedef struct
     char grid[ROWS][COLUMNS];
     char currentPlayer;
     int completedBoxes;
+    int scoreA;
+    int scoreB;
 
 } State;
 
@@ -61,4 +63,127 @@ bool isGameOver(State *game)
     int totalBoxes = ((ROWS - 1) / 2) * ((COLUMNS - 1) / 2);
 
     return game->completedBoxes >= totalBoxes;
+}
+
+bool checkForCompletedBoxes(State *game, int x1, int y1, int x2, int y2)
+{
+    int row1 = 2 * x1, col1 = 2 * y1;
+    int row2 = 2 * x2, col2 = 2 * y2;
+    bool boxCompleted = false;
+
+    bool isHorizontal = (row1 == row2);
+
+    if (isHorizontal)
+    {
+        int midRow = row1;
+        int midCol = (col1 + col2) / 2;
+
+        if (midRow > 1)
+        {
+            int boxRow = midRow - 1;
+            int boxCol = midCol;
+
+            if (game->grid[boxRow][boxCol - 1] != ' ' &&
+                game->grid[boxRow][boxCol + 1] != ' ' &&
+                game->grid[boxRow - 1][boxCol] != ' ')
+            {
+
+                game->grid[boxRow][boxCol] = game->currentPlayer;
+                boxCompleted = true;
+                game->completedBoxes++;
+
+                if (game->currentPlayer == 'A')
+                {
+                    game->scoreA++;
+                }
+                else
+                {
+                    game->scoreB++;
+                }
+            }
+        }
+
+        if (midRow < ROWS - 2)
+        {
+            int boxRow = midRow + 1;
+            int boxCol = midCol;
+
+            if (game->grid[boxRow][boxCol - 1] != ' ' &&
+                game->grid[boxRow][boxCol + 1] != ' ' &&
+                game->grid[boxRow + 1][boxCol] != ' ')
+            {
+
+                game->grid[boxRow][boxCol] = game->currentPlayer;
+                boxCompleted = true;
+                game->completedBoxes++;
+
+                if (game->currentPlayer == 'A')
+                {
+                    game->scoreA++;
+                }
+                else
+                {
+                    game->scoreB++;
+                }
+            }
+        }
+    }
+    else
+    {
+
+        int midRow = (row1 + row2) / 2;
+        int midCol = col1;
+
+        if (midCol > 1)
+        {
+            int boxRow = midRow;
+            int boxCol = midCol - 1;
+
+            if (game->grid[boxRow - 1][boxCol] != ' ' &&
+                game->grid[boxRow + 1][boxCol] != ' ' &&
+                game->grid[boxRow][boxCol - 1] != ' ')
+            {
+
+                game->grid[boxRow][boxCol] = game->currentPlayer;
+                boxCompleted = true;
+                game->completedBoxes++;
+
+                if (game->currentPlayer == 'A')
+                {
+                    game->scoreA++;
+                }
+                else
+                {
+                    game->scoreB++;
+                }
+            }
+        }
+
+        if (midCol < COLUMNS - 2)
+        {
+            int boxRow = midRow;
+            int boxCol = midCol + 1;
+
+            if (game->grid[boxRow - 1][boxCol] != ' ' &&
+                game->grid[boxRow + 1][boxCol] != ' ' &&
+                game->grid[boxRow][boxCol + 1] != ' ')
+            {
+
+                game->grid[boxRow][boxCol] = game->currentPlayer;
+                boxCompleted = true;
+                game->completedBoxes++;
+
+                if (game->currentPlayer == 'A')
+                {
+                    game->scoreA++;
+                }
+                else
+                {
+                    game->scoreB++;
+                }
+            }
+        }
+    }
+
+    return boxCompleted;
 }
