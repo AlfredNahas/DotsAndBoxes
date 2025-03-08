4f2a4ed385565929cd5633f3b46d6498401c4657
Jado233 <jna35@mail.aub.edu>
2025-03-08 20:33:10 +0200

    added "completedBoxes" value in the struct and added a bool method called "isGameOver" to check if the game is over or not

--- Game_Logic.c
+++ Game_Logic.c
@@ -1,48 +1,64 @@
-#include<stdio.h>
-#include<stdbool.h>
-
+#include <stdio.h>
+#include <stdbool.h>
 #define ROWS 5
 #define COLUMNS 6
 
-typedef struct {
+typedef struct
+{
     char grid[ROWS][COLUMNS];
     char currentPlayer;
-    
+    int completedBoxes;
+
 } State;
 
-
-bool isValidLine(int x1, int y1, int x2, int y2, char grid[ROWS][COLUMNS]) { 
+bool isValidLine(int x1, int y1, int x2, int y2, char grid[ROWS][COLUMNS])
+{
     int row1 = 2 * x1, col1 = 2 * y1;
     int row2 = 2 * x2, col2 = 2 * y2;
 
-    if (row1 < 0 || row1 >= ROWS || col1 < 0 || col1 >= COLUMNS || 
-        row2 < 0 || row2 >= ROWS || col2 < 0 || col2 >= COLUMNS) {
+    if (row1 < 0 || row1 >= ROWS || col1 < 0 || col1 >= COLUMNS ||
+        row2 < 0 || row2 >= ROWS || col2 < 0 || col2 >= COLUMNS)
+    {
 
-     return false; 
+        return false;
     }
-    if ((row1 == row2 && col2 == col1 + 2) ||  
-        (row1 == row2 && col2 == col1 - 2) ||  
-        (col1 == col2 && row2 == row1 + 2) ||  
-        (col1 == col2 && row2 == row1 - 2)) {
+    if ((row1 == row2 && col2 == col1 + 2) ||
+        (row1 == row2 && col2 == col1 - 2) ||
+        (col1 == col2 && row2 == row1 + 2) ||
+        (col1 == col2 && row2 == row1 - 2))
+    {
 
-
-        if (row1 != row2 && col1 != col2) {
-            return false; 
+        if (row1 != row2 && col1 != col2)
+        {
+            return false;
         }
         int midRow = (row1 + row2) / 2;
         int midCol = (col1 + col2) / 2;
 
-        if (grid[midRow][midCol] == ' ') {
-            return true;  
+        if (grid[midRow][midCol] == ' ')
+        {
+            return true;
         }
     }
-    return false;  
+    return false;
 }
 
-void switchPlayer(State *game) {
-    if (game->currentPlayer == 'A') {
+void switchPlayer(State *game)
+{
+    if (game->currentPlayer == 'A')
+    {
         game->currentPlayer = 'B';
-    } else {
+    }
+    else
+    {
         game->currentPlayer = 'A';
     }
+}
+
+bool isGameOver(State *game)
+{
+
+    int totalBoxes = ((ROWS - 1) / 2) * ((COLUMNS - 1) / 2);
+
+    return game->completedBoxes >= totalBoxes;
 }
